069295ffc6b809f3946661cac50b4b1f38ba24b7
Asım Emre Kaya <asimemre@serimsoft.local>
2021-08-02 13:27:00 +0300

    component did mount func added for axios

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
-import React, { Component } from 'react'
-import { Text, StyleSheet, View } from 'react-native'
+import React, {Component, useState} from 'react';
+import {Text, StyleSheet, View, Button} from 'react-native';
+import axios from 'axios';
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: [],
+    };
+  }
+  componentDidMount() {
+    this.getData();
+  }
+  async getData() {
+    let api = await axios.get('https://reactnative.dev/movies.json');
+    this.setState({movies: api.data.movies})
+    console.log(this.state.movies)
+  }
   render() {
     return (
       <View>
-        <Text> textInComponent </Text>
+        <Button
+          style={{fontSize: 20, color: 'green'}}
+          styleDisabled={{color: 'red'}}
+          onPress={() => this.setState({name: 'osman'})}
+          title="Press Me"></Button>
+        <Text> emre </Text>
       </View>
-    )
+    );
   }
 }
-
-const styles = StyleSheet.create({})
