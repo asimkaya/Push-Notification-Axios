382063e3fb79d9c8cf14c81f4180f78bf680657c
Asım Emre Kaya <asimemre@serimsoft.local>
2021-08-02 13:39:42 +0300

    flat list item added

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {Component, useState} from 'react';
-import {Text, StyleSheet, View, Button} from 'react-native';
+import {Text, StyleSheet, View, FlatList} from 'react-native';
 import axios from 'axios';
 
 export default class App extends Component {
@@ -14,18 +14,15 @@ export default class App extends Component {
   }
   async getData() {
     let api = await axios.get('https://reactnative.dev/movies.json');
-    this.setState({movies: api.data.movies})
-    console.log(this.state.movies)
+    this.setState({movies: api.data.movies});
+    console.log(this.state.movies);
   }
   render() {
     return (
       <View>
-        <Button
-          style={{fontSize: 20, color: 'green'}}
-          styleDisabled={{color: 'red'}}
-          onPress={() => this.setState({name: 'osman'})}
-          title="Press Me"></Button>
-        <Text> emre </Text>
+        <FlatList 
+          data={this.state.movies}
+          renderItem={({item}) => <Text>{item.title} {item.releaseYear}</Text>} />
       </View>
     );
   }
