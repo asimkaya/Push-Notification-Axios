9369755a7acc0eb00d8f83247f1252f4caa1ebef
Asım Emre Kaya <asimemre@serimsoft.local>
2021-08-02 15:28:22 +0300

    push natification added

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {Component, useState} from 'react';
-import {Text, StyleSheet, View, FlatList} from 'react-native';
+import {Text, StyleSheet, View, FlatList, Pressable} from 'react-native';
 import axios from 'axios';
+import OneSignal from 'react-native-onesignal';
 
 export default class App extends Component {
   constructor() {
@@ -9,21 +10,60 @@ export default class App extends Component {
       movies: [],
     };
   }
+
   componentDidMount() {
     this.getData();
+    OneSignal.setLogLevel(6, 0);
+    OneSignal.setAppId('bc1fa09c-3c90-4c45-b289-20a3cc1892ad');
   }
+
   async getData() {
-    let api = await axios.get('https://reactnative.dev/movies.json');
-    this.setState({movies: api.data.movies});
+    let api = await axios.get(
+      'https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations',
+    );
+    this.setState({movies: api.data.civilizations});
     console.log(this.state.movies);
   }
+
   render() {
     return (
       <View>
-        <FlatList 
+        <FlatList
           data={this.state.movies}
-          renderItem={({item}) => <Text>{item.title} {item.releaseYear}</Text>} />
+          renderItem={({item}) => (
+            <Pressable style={styles.button}>
+              <Text style={styles.text}>{item.name}</Text>
+            </Pressable>
+          )}
+        />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  listItem: {
+    backgroundColor: 'green',
+    margin: 5,
+    color: 'white',
+    fontSize: 20,
+    textAlign: 'center',
+    height: 40,
+  },
+  button: {
+    alignItems: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: '#001636',
+    margin: 5,
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+});
