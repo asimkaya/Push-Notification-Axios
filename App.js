98e54f1e0fb4b2920f1d3e33a7282b1f284f4363
Asım Emre Kaya <asimemre@serimsoft.local>
2021-08-05 15:48:22 +0300

    bottom stack navigator added

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import Home from './Pages/Home';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 const Stack = createNativeStackNavigator();
 
--- Pages/Detail.js
+++ Pages/Detail.js
@@ -7,8 +7,10 @@ function Detail({route, navigation}) {
     <View style={styles.container}>
       <View style={styles.square}>
         <Text style={styles.header}>{name}</Text>
-        <Image 
-          source={require('../assets/img/sword.png')} style={styles.sword} />
+        <Image
+          source={require('../assets/img/sword.png')}
+          style={styles.sword}
+        />
       </View>
       <Text style={styles.type}>Army Type</Text>
       <Text style={styles.army}>{type}</Text>
@@ -21,41 +23,41 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#CECEFF'
+    backgroundColor: '#CECEFF',
   },
   square: {
     width: 300,
     height: 400,
     backgroundColor: '#001636',
-    borderRadius: 30
+    borderRadius: 30,
   },
   header: {
     color: '#CFCFCF',
     fontSize: 30,
     textAlign: 'center',
     fontWeight: 'bold',
-    marginTop: 10,
-    fontFamily: 'monospace'
+    marginTop: 15,
+    fontFamily: 'monospace',
   },
   sword: {
     width: 100,
     height: 100,
     marginLeft: 'auto',
     marginRight: 'auto',
-    marginTop: 20
+    marginTop: 20,
   },
   type: {
     position: 'absolute',
     color: 'white',
     fontFamily: 'monospace',
-    fontSize: 15
+    fontSize: 15,
   },
   army: {
     position: 'absolute',
     color: 'red',
     fontFamily: 'monospace',
     fontSize: 25,
-    paddingTop: 100
-  }
-})
+    paddingTop: 130,
+  },
+});
 export default Detail;
